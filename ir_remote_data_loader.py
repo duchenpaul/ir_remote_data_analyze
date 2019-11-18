414f9d94162b3d4c966070113131a1ab405182f3
duchenpaul <duchenpaul@gmail.com>
2019-11-18 20:39:59 +0800

    Add mysql insert support

--- ir_remote_data_loader.py
+++ ir_remote_data_loader.py
@@ -1,10 +1,18 @@
+from sqlalchemy import create_engine, MetaData
 import pandas as pd
 
 import toolkit_sqlite
 
 import ir_remote_data_reader
 
-DB_FILE = 'test.db'
+DB_FILE = 'ir_remote_data.db'
+
+mysql_config = {
+    'user': 'pi',
+    'password': 'raspberry',
+    'server': 'rpi',
+    'database': 'test', 
+    }
 
 
 def insert_to_db(data_dict):
@@ -13,13 +21,25 @@ def insert_to_db(data_dict):
 
     df_dev = dict()
     df_dev['device_name'] = 'air_conditioner'
-    df_dev['rc_button'] = 'switch'
+    df_dev['rc_button'] = 'test'
     df_dev['rc_button_data'] = data_dict['data']
     df_dev = pd.DataFrame([df_dev])
-    
+
     with toolkit_sqlite.SqliteDB(DB_FILE) as sqlitedb:
-        df.to_sql('ir_remote_data', sqlitedb.conn, if_exists='append', index = False)
-        df_dev.to_sql('ir_remote_device', sqlitedb.conn, if_exists='append', index = False)
+        df.to_sql('ir_remote_data', sqlitedb.conn,
+                  if_exists='append', index=False)
+        df_dev.to_sql('ir_remote_device', sqlitedb.conn,
+                      if_exists='append', index=False)
+
+    engine = create_engine(
+        'mysql://{user}:{password}@{server}/{database}'.format(**mysql_config))
+
+    with engine.connect() as conn:
+        with conn.begin():
+            df.to_sql('ir_remote_data', conn,
+                      if_exists='append', index=False)
+            df_dev.to_sql('ir_remote_device', conn,
+                          if_exists='append', index=False)
 
 
 if __name__ == '__main__':
